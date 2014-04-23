de40269a42dc799692ae3abca99da81cac458a79
shaman203 <matyi204@gmail.com>
2014-04-23 22:48:38 +0200

    Added GUI prototype

--- EEG_Face/src/GUI/MainGUI.java
+++ EEG_Face/src/GUI/MainGUI.java
@@ -1,10 +1,96 @@
 package GUI;
 
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 public class MainGUI {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Running...");
+	private JPanel predictPanel;
+	private JPanel trainPanel;
+	private JTextArea predictLogTxtArea;
+	private JButton selectModelLocationBtn;
+	private JTextField modelFilenameField;
+	private JCheckBox saveLogToFileChckBox;
+	private JButton selectLogLocationBtn;
+	private JTextField logFilenameField;
+	private JButton connectBtn;
+	private JButton disconnectBtn;
+
+	public  MainGUI() {
+		JFrame guiFrame = new JFrame();
+
+		//make sure the program exits when the frame closes
+		guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		guiFrame.setTitle("Example GUI");
+		guiFrame.setSize(300,250);
+
+		//This will center the JFrame in the middle of the screen
+		guiFrame.setLocationRelativeTo(null);
+
+		JTabbedPane panes = new JTabbedPane();
+
+		predictPanel = new JPanel();
+		trainPanel = new JPanel();
+
+		panes.addTab("Predict", predictPanel);
+		panes.addTab("Train", trainPanel);
+
+
+		GroupLayout predictLayout = new GroupLayout(predictPanel);
+		predictPanel.setLayout(predictLayout);
+
+		predictLayout.setAutoCreateGaps(true);
+		predictLayout.setAutoCreateContainerGaps(true);
+
+		predictLogTxtArea = new JTextArea("Here will the events appear");
+		selectModelLocationBtn = new JButton("Load SVM...");
+		modelFilenameField = new JTextField("SVM model");
+
+		saveLogToFileChckBox = new JCheckBox("Save log");
+		selectLogLocationBtn = new JButton("Log location...");
+		logFilenameField =new JTextField("Log file"); 
+
+		connectBtn = new JButton("Connect");
+		disconnectBtn = new JButton("Disconnect");
+
+		predictLayout.setHorizontalGroup(
+				predictLayout.createParallelGroup()				      
+				.addComponent(predictLogTxtArea)
+				.addGroup(predictLayout.createSequentialGroup()
+						.addComponent(modelFilenameField)
+						.addComponent(selectModelLocationBtn))
+				.addGroup(predictLayout.createSequentialGroup()
+						.addComponent(saveLogToFileChckBox)
+						.addComponent(logFilenameField)
+						.addComponent(selectLogLocationBtn))
+				.addGroup(predictLayout.createSequentialGroup()
+						.addComponent(connectBtn)
+						.addComponent(disconnectBtn))
+				);
+		predictLayout.setVerticalGroup(
+				predictLayout.createSequentialGroup()
+				.addComponent(predictLogTxtArea)
+				.addGroup(predictLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+						.addComponent(modelFilenameField)
+						.addComponent(selectModelLocationBtn))
+				.addGroup(predictLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+						.addComponent(saveLogToFileChckBox)
+						.addComponent(logFilenameField)
+						.addComponent(selectLogLocationBtn))
+				.addGroup(predictLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+						.addComponent(connectBtn)
+						.addComponent(disconnectBtn))
+				);
+
+		guiFrame.add(panes);
+		//make sure the JFrame is visible
+		guiFrame.setVisible(true);
 	}
 
 }
---
+++ EEG_Face/src/test/Prog.java
@@ -0,0 +1,12 @@
+package test;
+
+import GUI.MainGUI;
+
+public class Prog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new MainGUI();
+	}
+
+}
